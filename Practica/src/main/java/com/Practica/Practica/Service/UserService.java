3e6c6bd7a1fd9d047a8cff5e2b2a871aa1772620
Braian Rojas <braianrojas95@gmail.com>
2025-05-15 20:24:51 -0300

    test: agrega test de saveUser con Mockito y configura H2 para entorno de pruebas

--- Practica/src/main/java/com/Practica/Practica/Service/UserService.java
+++ Practica/src/main/java/com/Practica/Practica/Service/UserService.java
@@ -25,12 +25,14 @@ public class UserService {
 
 
     public Optional<User> getUserById(Long id){
-      return userRepository.findById(id);
+        return userRepository.findById(id);
     };
 
-    
 
 
+    public void deleteUser(User user){
+        userRepository.delete(user);
+    }
 
 
 
---
+++ Practica/src/test/java/com/Practica/Practica/Service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package com.Practica.Practica.Service;
+
+import com.Practica.Practica.Model.User;
+import com.Practica.Practica.Repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+
+    @Test
+    void shouldSaveUser() {
+        User user = new User(null,"Braian","1234");
+        User savedUser= new User(1L,"Braian","1234");
+
+        when(userRepository.save(user)).thenReturn(savedUser);
+
+        User result = userService.saveUser(user);
+
+        assertNotNull(result);
+        assertEquals(1L,result.getId());
+        assertEquals("Braian",result.getName());
+        assertEquals("1234",result.getPassword());
+        verify(userRepository).save(user);
+
+    }
+
+    @Test
+    void shouldGetAllUsers() {
+    }
+
+    @Test
+    void shouldGetUserById() {
+    }
+
+    @Test
+    void shouldDeleteUser() {
+    }
+}
